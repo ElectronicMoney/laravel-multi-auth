ef88ee9f2f764612c2fcc7f0cd1daaa1beaadb27
Emeka Augustine <cafafanscoders@gamil.com>
2019-01-20 04:12:15 -0500

    Admin login setup

---
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Auth;
+
+class AdminLoginController extends Controller
+{
+    public function __construct() {
+        $this->middleware('guest:admin');
+    }
+
+    //Show admin login form
+    public function showAdminLoginForm() {
+        return view('admin.auth.login');
+    }
+
+    //Admin Login
+    public function adminLogin(Request $request) {
+        // 1. Validate the form data
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+
+        // 2. Attempt to login the admin user
+        if (auth()->guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->remember)) {
+            // 3. If successful; redirect the user to its destination.
+            return redirect()->intended(route('admin.dashboard'));
+        }
+        // 4. Else, redirect them back to the admin login page
+        return back()->withErrors(['email' => 'Email or password are wrong.']);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,22 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
+
+//Admin Login
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('/login', 'Auth\AdminLoginController@showAdminLoginForm')->name('get.admin.login');
+    Route::post('/login', 'Auth\AdminLoginController@adminLogin')->name('post.admin.login');
+    Route::get('/dashboard', 'AdminController@index')->name('admin.dashboard');
+});
+
+//Admin Auth Routes
+// Route::group(['prefix' => 'admin'], function() {
+//     Auth::routes();
+//     Route::get('/home', 'AdminController@index');
+// });
+
+
+// Route::get('/admin', 'AdminController@index')->name('admin');
